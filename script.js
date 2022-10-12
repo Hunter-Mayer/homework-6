b82c71a059eb9b9fdb5f9a64d7a882a84a59036f
Hunter-Mayer <hmayer95@gmail.com>
2022-10-12 13:26:11 -0500

    Got fetch's and search function working

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 var APIKey = "21608b13cfc9360b12231cf7c4d067a6"
 var searchBtn = $('#searchBtn')
-var cityInput = $('#input')
-var url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + "&units=imperial&appid=" + APIKey
+var input = $('#input')
 
 function search(){
-    var city = cityInput.value()
-    //function renderWeather
-    //other functions here
+    var inputValue = input.val()
+    console.log(inputValue)
+    renderWeather(inputValue)
+    renderForecast(inputValue)
 }
 
+searchBtn.on('click', search)
 
-//function renderWeather(city) {
-    // fetch a city's 5 day forecast by searching it
+function renderWeather(input) {
+    // fetch a city's current weather by searching it
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + input + "&appid=" + APIKey + "&units=imperial"
     fetch (url)
       .then(function(response) {
         return response.json()
       })
       .then(function(data){
         console.log(data)
-      }) 
-    //}
+        
+        })
+}
+
+function renderForecast(input) {
+  //fetch a city's five day forecast by searching it
+  var forcastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + APIKey + "&units=imperial"
+  fetch (forcastUrl)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data2){
+        console.log(data2)
+})
+}
