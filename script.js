7fe9337e49fb7cabd6e501b83a699044bad782dc
Hunter-Mayer <hmayer95@gmail.com>
2022-10-12 14:29:50 -0500

    got the five day forecast working

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ var input = $('#input')
 var currentTempEl = document.getElementById('temperature')
 var currentWindSpeedEl = document.getElementById('wind')
 var currentHumidityEl = document.getElementById('humidity')
+var fivedayForecast = document.getElementById('forecast')
+// var date = document.getElementById('date')
+// var forecastTempEl = document.getElementById('temperature1')
+// var forecastWindspeedEl = document.getElementById('wind1')
+// var forecastHumidityEl = document.getElementById('humidity1')
 
 function search(){
   //adds functionality to the search button, runs the functions created when search is clicked
@@ -27,20 +32,38 @@ function renderWeather(input) {
         var temperature = data.main.temp
         var wind = data.wind.speed
         var humidity = data.main.humidity
-          currentTempEl.textContent = "Tempature in " + input + " is " + temperature + " °F"
-          currentWindSpeedEl.textContent = "Wind speed in " + input + " is " + wind + " MPH"
-          currentHumidityEl.textContent = "Humidity in " + input + " is " + humidity + " %"
+          currentTempEl.textContent = "Current tempature in " + input + " is " + temperature + " °F"
+          currentWindSpeedEl.textContent = "Current wind speed in " + input + " is " + wind + " MPH"
+          currentHumidityEl.textContent = "Current humidity in " + input + " is " + humidity + " %"
         })
 }
 
 function renderForecast(input) {
   //fetch a city's five day forecast by searching it
-  var forcastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + APIKey + "&units=imperial"
-  fetch (forcastUrl)
+  var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + APIKey + "&units=imperial"
+  fetch (forecastUrl)
       .then(function(response) {
         return response.json()
       })
       .then(function(data2){
         console.log(data2)
+    for (var i = 3; i < data2.list.length; i+=9){
+            var forecast1 = document.createElement('div');
+            var date = document.createElement('p')
+            var temperature = document.createElement('p')
+            var wind = document.createElement('p')
+            var humidity = document.createElement('p')
+
+            date.textContent = moment.unix(data2.list[i].dt).format("MM-DD-YYYY")
+            temperature.textContent = "Temp: " + data2.list[i].main.temp + " °F"
+            wind.textContent = "Wind: " + data2.list[i].wind.speed + " MPH"
+            humidity.textContent = "Humidity: " + data2.list[i].main.humidity + " %"
+
+            fivedayForecast.append(forecast1);
+            forecast1.append(date);
+            forecast1.append(temperature);
+            forecast1.append(wind);
+            forecast1.append(humidity);
+    }
 })
 }
